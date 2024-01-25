6a677efc0be248e3bc03af6fd32c4c4743a54da1
Walter Miller <waltermil@gmail.com>
2024-01-25 08:05:39 -0500

    refactor

--- app/server.go
+++ app/server.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"os"
-	"regexp"
 	"strings"
+	"sync"
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
 )
 
-var db = make(map[string]string)
+var db = struct{
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
+
+
+type RedisCommand struct {
+	name string
+	args []string
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -33,50 +41,54 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn)
+		go newHandleConnection(conn)
 	}
 }
 
+// a function that parses redis commands of the form "*<number of arguments>\r\n$<length of command>\r\n<command>\r\n"
+// and returns a RedisCommand struct
+func parseCommand(command string) RedisCommand {
+	cmd := RedisCommand{}
+	chunks := strings.Split(command, "\r\n")
+	args := make([]string, 0)
+	cmd.name = strings.ToUpper(chunks[2])
+	for i := 4; i < len(chunks); i += 2 {
+		args = append(args, chunks[i])
+	}
+	cmd.args = args
+	return cmd
+}
 
-func handleConnection(conn net.Conn) {
-	alpha, _ := regexp.Compile("^[a-zA-Z]")
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-		if strings.ToUpper(scanner.Text()) == "PING" {
+func newHandleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	for {
+		readLen, err := conn.Read(buffer)
+		if readLen == 0 || err != nil && err.Error()  == "EOF" {
+			continue
+		}
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			continue
+		}
+		command := parseCommand(string(buffer))
+		switch command.name {
+		case "PING":
 			conn.Write([]byte("+PONG\r\n"))
-		} else if strings.ToUpper(scanner.Text()) == "ECHO" {
-			
-			for scanner.Scan() {
-				if alpha.MatchString(scanner.Text()) {
-					conn.Write([]byte("+" + scanner.Text() + "\r\n"))
-					break
-				} else {
-					fmt.Println(scanner.Text())
-				}
-			}
-		} else if strings.ToUpper(scanner.Text()) == "SET" {
-			scanner.Scan()
-			// skip $ length
-			scanner.Scan()
-			key := scanner.Text()
-			scanner.Scan()
-			// skip $ length
-			scanner.Scan()
-			value := scanner.Text()
-			db[key] = value
+		case "ECHO":
+			conn.Write([]byte("+" + command.args[0] + "\r\n"))
+		case "SET":
+			db.Lock()
+			db.m[command.args[0]] = command.args[1]
+			db.Unlock()
 			conn.Write([]byte("+OK\r\n"))
-		} else if strings.ToUpper(scanner.Text()) == "GET" {
-			scanner.Scan()
-			// skip $ length
-			scanner.Scan()
-			key := scanner.Text()
-			if val, ok := db[key]; ok {
+		case "GET":
+			db.RLock()
+			if val, ok := db.m[command.args[0]]; ok {
 				conn.Write([]byte("$" + fmt.Sprint(len(val)) + "\r\n" + val + "\r\n"))
 			} else {
 				conn.Write([]byte("$-1\r\n"))
 			}
-		}
-
+			db.RUnlock()
+		} 
 	}
 }
